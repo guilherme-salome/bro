92f350e2c03e7cbf6c9199e9b082c196abe2400c
Guilherme Salome <ferreira_pelucio_salome_guilherme@lilly.com>
2024-01-08 02:14:42 +0000

    source nodes from __nodes__ list

--- R/load_nodes.R
+++ R/load_nodes.R
@@ -1,24 +1,23 @@
-#' @title Load Nodes from Configuration File
+#' @title Load Nodes from a Script File
 #'
 #' @description
-#' Loads nodes from a specified configuration file into a separate environment.
+#' Loads nodes and other objects, including functions, from a specified script file into a separate environment.
 #'
 #' @details
-#' This function takes the path to a configuration file (default is "R/nodes.R") and creates a new environment (`objects`).
-#' It then sources the specified file, executing its contents within the new environment. The function selects only objects
-#' with the class 'node' from the environment, which are typically used to represent nodes in a data processing or analysis
-#' pipeline. These nodes can include various data processing functions or other objects relevant to the workflow.
+#' This function takes a file path as input, which defaults to "R/nodes.R". It creates a new environment and sources the
+#' script file within this environment. All objects, including functions, are stored in this separate environment.
+#' The function then retrieves and returns only the objects with the 'node' class from the created environment.
+#' These nodes are extracted from a dunder variable named "__nodes__", that is created when the "nodes.R" file
+#' is sourced.
 #'
-#' @param path (string) The path to the configuration file containing nodes and other objects.
+#' @param path The file path to the script containing nodes and other objects (default: "R/nodes.R").
 #'
-#' @return Returns a list of nodes loaded from the specified configuration file.
+#' @return Returns the objects with the 'node' class loaded from the specified script file.
 #'
 load_nodes <- function(path = file.path("R", "nodes.R")) {
   ## Load nodes and possibly other objects, including functions, into a separate environment
   objects <- new.env()
   source(path, chdir = TRUE, local = objects)
   ## Select only objects with 'node' class
-  classes <- lapply(mget(ls(envir = objects), envir = objects), class)
-  nodes <- mget(names(classes)[classes == "node"], envir = objects)
-  return(nodes)
+  return(get("__nodes__", envir = objects))
 }
